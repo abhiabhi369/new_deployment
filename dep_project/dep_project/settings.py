009b8f15acd24d433fb948943a33c9a0e88b6a24
abhilash <abhilashgangula370@gmail.com>
2021-03-25 16:47:35 +0530

    -

--- dep_project/dep_project/settings.py
+++ dep_project/dep_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,4 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
--- dep_project/dep_project/urls.py
+++ dep_project/dep_project/urls.py
@@ -16,10 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
-from django.conf.urls import url
-from django.conf.urls.static import static
-from django.views.static import static
-from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
