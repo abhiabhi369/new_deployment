e505d24885c51317067ce867e2d0d6509ca8ff7a
abhilash <abhilashgangula370@gmail.com>
2021-03-25 13:57:31 +0530

    asdf

--- dep_project/dep_project/urls.py
+++ dep_project/dep_project/urls.py
@@ -19,6 +19,7 @@ from . import views
 from django.conf.urls import url
 from django.conf.urls.static import static
 from django.views.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +27,4 @@ urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
+urlpatterns=url+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
